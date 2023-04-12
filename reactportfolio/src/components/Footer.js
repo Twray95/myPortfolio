387f64617d1c586243b2573bb2f93efb46f39f62
Thomas Wray <twray95@gmail.com>
2023-04-12 13:59:11 -0600

    removed react-bootstrap

---
+++ reactportfolio/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="align-items-end">
+        Here is will I put icons for LI, GH, and Email
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- reactportfolio/src/components/NavTabs.js
+++ reactportfolio/src/components/NavTabs.js
@@ -5,23 +5,28 @@ import { Link } from "react-router-dom";
 // We assign them to their own variable names
 function NavTabs() {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/About">
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/Contact">
-          Contact
-        </Link>
-      </li>
-    </ul>
+    <>
+      <div className="row text-center">
+        <div className="col">Thomas Wray</div>
+        <ul className="nav nav-tabs col justify-content-end">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Work">
+              Work
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
