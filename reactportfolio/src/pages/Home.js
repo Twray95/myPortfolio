700e5ec11fbfc60a2cf48b91a03a1da01bfa9981
Thomas Wray <twray95@gmail.com>
2023-07-12 11:35:38 -0600

    added image of me and worked on css of homepage

--- reactportfolio/src/pages/Home.js
+++ reactportfolio/src/pages/Home.js
@@ -4,15 +4,16 @@ function Home() {
   return (
     <>
       <div className="row">
-        <div className="col-lg-8">
-          <main>Hey, my name is Thomas Wray</main>
-          <section>
-            I am a burgeoning full-stack web developer. I love to work with
-            Node.js, Express, Sequelize, SQL, MongoDB, GraphQl, and much more.
+        <div className="col-lg-8 align-middle">
+          <main className="text-center">Hello, my name is Thomas Wray</main>
+          <section className="text-center align-bottom">
+            I am a new full stack developer and I'm excited to learn and grow in
+            this field. Some of the apps that I know include Node.js, Express,
+            Sequelize, SQL, MongoDB, GraphQl, and much more.
           </section>
         </div>
-        <div className="col-lg-4">
-          <div>image goes here</div>
+        <div className="col-lg-4 mainImgCon">
+          <img className="meImg" src="me.png"></img>
         </div>
       </div>
     </>
